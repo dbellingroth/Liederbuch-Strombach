a563ffa0073b476339dd3f8b6e5799c3c238d370
David Bellingroth <dbellingroth@gmail.com>
2023-03-06 21:22:54 +0100

    Erster Commit

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+xelatex "Liederbuch Strombach.tex"
+texindy -C utf8 -M inhalt.xdy "Liederbuch Strombach.idx"
+xelatex "Liederbuch Strombach.tex"
+rm *.aux
+rm "Liederbuch Strombach.out"
+rm "Liederbuch Strombach.ilg"
+rm "Liederbuch Strombach.ind"
+rm "Liederbuch Strombach.log"
+rm "Liederbuch Strombach.idx"
+rm inhalt.xdy
